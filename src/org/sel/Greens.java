74ca7e0a842156d131d07f6b8672761db6328e3b
bindugit <bindu.gadaparthi@gmail.com>
2019-09-10 09:56:03 +0530

    command

---
+++ src/org/sel/Greens.java
@@ -0,0 +1,18 @@
+package org.sel;
+
+public class Greens  extends Inheritane {
+	public void sub(){
+	System.out.println("class");
+}
+public static void main(String[]args) {
+	Greens g=new Greens();
+	g.sub();
+	g.add();
+	
+}
+
+
+}
+
+
+
